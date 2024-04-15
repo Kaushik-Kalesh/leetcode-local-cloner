507aa6222dec16de624e68804383d86d414dad1a
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-04-15 15:42:27 +0530

    minor changes

--- cloner.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'startClone') {
-
-    }
-    else if (message.action == 'removeFiles') {
-
-    }
-})
