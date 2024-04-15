ef27082b37969ffc92c007b4c37a468ab418c52b
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-04-15 12:25:32 +0530

    added background worker script

---
+++ cloner.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'startClone') {
+
+    }
+    else if (message.action == 'removeFiles') {
+
+    }
+})
