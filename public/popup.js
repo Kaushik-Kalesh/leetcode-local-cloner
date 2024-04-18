f40a626eda1c99c4588607c96b0dbafd07e28d29
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-04-18 12:12:54 +0530

    formatted code

--- public/popup.js
+++ public/popup.js
@@ -19,34 +19,37 @@ function html2text(html) {
   return tag.innerText;
 }
 
+function downloadZIP(data) {
+  try {
+    const files = [
+      { name: "question.txt", content: html2text(data.questionContent) },
+      { name: "solution.c", content: data.langs.C },
+    ];
+
+    const zip = new JSZip();
+
+    files.forEach((file) => {
+      zip.file(file.name, file.content);
+    });
+
+    zip.generateAsync({ type: "blob" }).then((blob) => {
+      const url = URL.createObjectURL(blob);
+
+      // Dynamic download link creation
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = data.title + ".zip";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  } catch (err) {
+    error(err);
+  }
+}
+
 navigator.serviceWorker.addEventListener("message", (event) => {
   if (event.data.message === "Download Zip") {
-    try {
-      const data = event.data;
-      const files = [
-        { name: "question.txt", content: html2text(data.questionContent) },
-        { name: "solution.c", content: data.langs.C },
-      ];
-
-      const zip = new JSZip();
-
-      files.forEach((file) => {
-        zip.file(file.name, file.content);
-      });
-
-      zip.generateAsync({ type: "blob" }).then((blob) => {
-        const url = URL.createObjectURL(blob);
-
-        // Dynamic download link creation
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = data.title + ".zip";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      });
-    } catch (err) {
-      error(err);
-    }
+    downloadZIP(event.data);
   }
 });
