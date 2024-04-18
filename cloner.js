5cf340557babf5ce71cae722bc3085a96f697654
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-04-18 11:43:24 +0530

    setup zip download

--- cloner.js
+++ cloner.js
@@ -1,59 +1,50 @@
 async function getCurrentTabUrl() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [ tab ] = await chrome.tabs.query(queryOptions);
-    return tab.url;
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
 }
 
 async function getData() {
   const PROXY_URL = "http://localhost:3000";
-  let lcQuestionURL = await getCurrentTabUrl();  
-  let lcQuestionURLSplit = lcQuestionURL.slice(0, -1).split('/')
+  let lcQuestionURL = await getCurrentTabUrl();
+  let lcQuestionURLSplit = lcQuestionURL.slice(0, -1).split("/");
 
-  if(lcQuestionURLSplit[2] !== 'leetcode.com') { return null; }
+  if (lcQuestionURLSplit[2] !== "leetcode.com") {
+    return null;
+  }
 
-  if(lcQuestionURLSplit[lcQuestionURLSplit.length - 1] === 'description') {
+  if (lcQuestionURLSplit[lcQuestionURLSplit.length - 1] === "description") {
     lcQuestionURLSplit.pop();
   }
   let lcQuestionTitle = lcQuestionURLSplit.pop();
 
-  const resData = {};
+  const resData = {
+    title: lcQuestionTitle,
+    questionContent: "",
+    testcases: [],
+    langs: {},
+  };
   try {
-    // const [questionContentRes, exampleTestcasesRes, codeSnippetsRes] = await Promise.all([
-    //   fetch(PROXY_URL + "/content/" + lcQuestionTitle),
-    //   fetch(PROXY_URL + "/testcases/" + lcQuestionTitle),
-    //   fetch(PROXY_URL + "/code-snippets/" + lcQuestionTitle),
-    // ]);
+    const [questionContentRes, exampleTestcaseRes, codeSnippetsRes] = await Promise.all([
+      fetch(PROXY_URL + "/content/" + lcQuestionTitle),
+      fetch(PROXY_URL + "/testcases/" + lcQuestionTitle),
+      fetch(PROXY_URL + "/code-snippets/" + lcQuestionTitle),
+    ]);
+
+    const questionContentData = await questionContentRes.json();
+    resData.questionContent = questionContentData.data.question.content;
+
+    const exampleTestcaseData = await exampleTestcaseRes.json();
+    exampleTestcaseData.data.question.exampleTestcaseList.forEach((e) => {
+      let [inp, out] = e.split("\n");
+      resData.testcases.push({ input: inp, expected_output: out });
+    });
+
     
-    // const questionContentData = await questionContentRes.json();
-    // const exampleTestcasesData = await exampleTestcasesRes.json();
-    // const codeSnippetsData = await codeSnippetsRes.json();
-
-    fetch(PROXY_URL + "/content/" + lcQuestionTitle)
-      .then(res => res.json())
-      .then(data => {
-        resData.questionContent = data.data.question.content;
-      })
-
-
-    fetch(PROXY_URL + "/testcases/" + lcQuestionTitle)
-      .then(res => res.json())
-      .then(data => {
-        resData.testcases = [];
-        data.data.question.exampleTestcaseList.forEach(e => {
-          let [inp, out] = e.split('\n');
-          resData.testcases.push({ input: inp, expected_output: out });
-        });
-      })
-
-
-    fetch(PROXY_URL + "/code-snippets/" + lcQuestionTitle)
-    .then(res => res.json())
-    .then(data => {
-      resData.langs = {};
-      data.data.question.codeSnippets.forEach(e => {
-        resData.langs[e.lang] = e.code;
-      });
-    })
+    const codeSnippetsData = await codeSnippetsRes.json();
+    codeSnippetsData.data.question.codeSnippets.forEach((e) => {
+      resData.langs[e.lang] = e.code;
+    });
 
     return resData;
   } catch (err) {
@@ -64,8 +55,15 @@ async function getData() {
 
 async function StartCloning() {
   const data = await getData();
-  console.log(data);
-  //TODO: Create necessary files in the local file system
+
+  self.clients.matchAll().then((clients) => {
+    clients.forEach((client) => {
+      client.postMessage({
+        message: "Download Zip",
+        ...data
+      });
+    });
+  });
 }
 
 async function RemoveClonedFiles() {
@@ -74,14 +72,19 @@ async function RemoveClonedFiles() {
 
 // Connect with frontend
 async function listenForMessages() {
-  await chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-      if (message.action === 'startClone') {        
+  await chrome.runtime.onMessage.addListener(
+    async (message, sender, sendResponse) => {
+      if (message.action === "startClone") {
         await StartCloning();
-      }
-      else if (message.action == 'removeFiles') {
+      } else if (message.action === "removeFiles") {
         await RemoveClonedFiles();
+      } else if (message.action === "Debug") {
+        console.log(message.log);
+      } else if (message.action === "Error") {
+        console.error(message.error);
       }
-  })
+    }
+  );
 }
 
-listenForMessages()
+listenForMessages();
--- public/popup.js
+++ public/popup.js
@@ -1,7 +1,47 @@
-document.getElementById('cloneButton').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'startClone' });
+document.getElementById("cloneButton").addEventListener("click", () => {
+  chrome.runtime.sendMessage({ action: "startClone" });
 });
 
-document.getElementById('removeButton').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'removeFiles' });
+document.getElementById("removeButton").addEventListener("click", () => {
+  chrome.runtime.sendMessage({ action: "removeFiles" });
+});
+
+function debug(msg) {
+    chrome.runtime.sendMessage({ action: "Debug", log: msg });        
+}
+function error(err) {
+    chrome.runtime.sendMessage({ action: "Error", error: err.message });        
+}
+
+
+navigator.serviceWorker.addEventListener("message", (event) => {
+  if (event.data.message === "Download Zip") {
+    try {
+      const data = event.data;
+      const files = [
+        { name: "question.txt", content: data.questionContent },
+        { name: "solution.c", content: data.langs.C }
+      ];
+
+      const zip = new JSZip();
+
+      files.forEach((file) => {
+        zip.file(file.name, file.content);
+      });
+
+      zip.generateAsync({ type: "blob" }).then((blob) => {
+        const url = URL.createObjectURL(blob);
+
+        // Dynamic download link creation
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = data.title + ".zip";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    } catch (err) {
+      error(err);
+    }
+  }
 });
